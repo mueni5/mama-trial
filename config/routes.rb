982d2bec245618b5894e0f1e97a91bd79537ab5b
+ <+>
2023-08-02 17:29:43 +0300

    config:routes -create messages route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :messages, only: [:index, :show, :create]
+  resources :messages
+  # get '/messages',  to: "messages#show"
+  # post '/messages', to: 'messages#create'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
