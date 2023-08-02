e81e9339b83ddfc9476c67672757f96e359b0221
+ <+>
2023-08-02 17:31:41 +0300

    database:seed -create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Seeding mothers...'
+
+Mother.create!(name: 'Zahra', age: 20, email: 'zahra@gmail.com')
+Mother.create!(name: 'Grackle', age: 32, email: 'grack@gmail.com')
+Mother.create!(name: 'Bree', age: 28, email: 'bree@example.com')
+Mother.create!(name: 'Dovey', age: 39, email: 'dovey@gmail.com')
+
+puts 'Seeding messages...'
+
+# Assuming you have the association set up between Message and Mother
+zahra = Mother.find_by(name: 'Zahra')
+grackle = Mother.find_by(name: 'Grackle')
+bree = Mother.find_by(name: 'Bree')
+dovey = Mother.find_by(name: 'Dovey')
+
+Message.create!(content: 'Scheduled for tests', sender: 'Zahra', mother: zahra)
+Message.create!(content: 'Doing well', sender: 'Grackle', mother: grackle)
+Message.create!(content: 'Delivering soon', sender: 'Bree', mother: bree)
+Message.create!(content: 'Having nausea since yesterday', sender: 'Dovey', mother: dovey)
+
+puts '✅ Done seeding!'
